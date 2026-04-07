718990d1326fa598d1e86c5d87a1904459f4c9f9
PushpenderRana <pushpenderrana270@gmail.com>
2026-04-07 20:23:17 +0530

    Delete portfolio directory

--- portfolio/contact/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from contact.models import contact
-
-# Register your models here.
-class contactadmin(admin.ModelAdmin):
-    list_display=('contact_name','contact_email','contact_message')
-admin.site.register(contact,contactadmin)
